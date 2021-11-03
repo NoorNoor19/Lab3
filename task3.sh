f7f2e1e870778324f08920d9b369977364b567cd
Mahnoor Bibi <56926453+NoorNoor19@users.noreply.github.com>
2021-11-03 10:25:00 -0400

    Update task3.sh

--- task3.sh
+++ task3.sh
@@ -1,5 +1,6 @@
-while IFS=, read -r Height ; do
-if [[ $Height > "175" ]] 
+Benchmark=190
+while IFS=, read -r title Author Genre Height Publisher ; do
+if [[ $Height > "175" ]] && [[ $Height < "230" ]]
 then echo $Height
 fi
 done < books.csv
